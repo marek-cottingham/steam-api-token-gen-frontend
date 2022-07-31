ebc4b9e4941acc4b781e18e452bdb88f95cf9741
Marek Cottingham <36125218+marek-cottingham@users.noreply.github.com>
2022-07-31 14:01:18 +0100

    Added search pending notice

--- src/App.js
+++ src/App.js
@@ -10,18 +10,25 @@ const App = () => {
 
   var [achivements, setAchivements] = useState([]);
   var [wasSearchError, setWasSearchError] = useState(false);
+  var [searchPending, setSearchPending] = useState(false);
 
   var onSearchSubmit = async (searchText) => {
+    
+      setSearchPending(true);
+      setWasSearchError(false);
+      
       const response = await axios.get('/getAchievementList', {
           params: {userName: searchText}
       }).catch(error => {
           console.log(error);
           setWasSearchError(true);
       })
+
+      setSearchPending(false);
       
       if (response !== undefined && response.data.achievements !== undefined) {
         setAchivements(response.data.achievements);
-        setWasSearchError(false);
+        
       }
   }
 
@@ -29,11 +36,19 @@ const App = () => {
     <div className="App">
       <h1>Steam achivements lookup</h1>
       <SearchBar onSearchSubmit={onSearchSubmit}></SearchBar>
+
       {wasSearchError ? (
         <div class="ui secondary inverted red segment">
           <p>Lookup error. User name may be invalid.</p>
         </div>
       ) : null}
+
+      {searchPending ? (
+        <div class="ui secondary inverted blue segment">
+          <p>Searching...</p>
+        </div>
+      ) : null}
+
       <div>
         {achivements.map(achivement => (<div key={achivement}>{achivement}</div>))}
       </div>
